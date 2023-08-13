e046a993aee67b4df82c1bef765d89270aa5f08a
sunny <rounsunn@gmail.com>
2023-08-13 17:52:33 +0530

    Moved to TS with express, in  next step - automate the process of build, start server and automatically re starting of server on change in index.ts file - using nodemon and npm run scripts

---
+++ express-ts/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 8000;
+app.get("/users", (req, res) => {
+    res.send("Hello World! from express");
+});
+app.listen(port, () => {
+    console.log("server app listening on port 8000!");
+});
---
+++ express-ts/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+const app = express();
+const port = 8000;
+
+app.get("/users", (req, res) => {
+  res.send("Hello World! from express");
+});
+
+app.listen(port, () => {
+  console.log("server app listening on port 8000!");
+});
